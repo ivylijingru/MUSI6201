65ddf7458a8f6aef1bd73900346f7b142f765086
22ananya <ananya22@vt.edu>
2023-09-17 14:27:32 -0400

    updated with added functions of spectral crest etc

--- A2/A2_module.py
+++ A2/A2_module.py
@@ -12,21 +12,12 @@ fs = 44.1e3 # sampling rate
 block_size = 1024 # block size
 hop_size = 512 # hop size
 
-# create a sample signal
-t = np.arange(0,1,1/fs) # time vector
-x = np.sin(2*np.pi*440*t) #+ np.sin(2*np.pi*880*t) + np.sin(2*np.pi*1320*t)  # signal vector
-
-#
-# Function 1 - Calculate Spectral Centroid
-# First block an input signal
-xb, timeInSec = block_audio(x,block_size, hop_size, fs)
-NumOfBlocks = len(timeInSec)
 
 # create spectral centroid function - takes blocked audio and sampling rate as input
 def extract_spectral_centroid(xb, fs):
     # calculate fft block by block, keeping only the positive frequencies and normalizing by the number of samples
     # create a frequency vector
-    b_size, nblocks = np.shape(xb)
+    nblocks, b_size = np.shape(xb)
     f = np.arange(0,fs/2 + fs/b_size,fs/b_size)
     flen = len(f)
     hw = np.hanning(flen) # hanning window
@@ -42,13 +33,56 @@ def extract_spectral_centroid(xb, fs):
 
 # calculate RMS energy in dB
 def extract_rms(xb):
-    b_size, nblocks = np.shape(xb) # get size of input vector
+    nblocks, b_size = np.shape(xb) # get size of input vector
     rms_dB = np.zeros(nblocks) # spectral centroid
     for i in range(nblocks):
-        rms_dB[i] = np.maximum(20*np.log10(np.sqrt(np.sum(np.square(xb[i,:])/np.ndarray.size(xb[i,:])))), -100)
+        rms_dB[i] = np.maximum(20*np.log10(np.sqrt(np.sum(np.square(xb[i,:])/np.size(xb[i,:])))), -100)
     return rms_dB
         
 
+
+# Create a function for extracting zero_crossings
+def extract_zerocrossingrate(xb,fs):
+    # loop over each block and find zero crossing rate by finding negative difference values
+    nblocks, b_size = np.shape(xb) # get size of input vector
+    zcr = np.zeros(nblocks) # spectral centroid
+    for i in range(nblocks):
+        zcr[i] = (1/(2*b_size))*np.sum( np.abs(np.diff(np.sign(xb[i,:]))))
+    return zcr
+
+# create a function to calculate spectral crest
+def extract_spectral_crest(xb, fs):
+    # calculate fft block by block, keeping only the positive frequencies and normalizing by the number of samples
+    # create a frequency vector
+    nblocks, b_size = np.shape(xb)
+    f = np.arange(0,fs/2 + fs/b_size,fs/b_size)
+    flen = len(f)
+    hw = np.hanning(flen) # hanning window
+
+    X_fft = np.zeros((nblocks, flen)) # FFT vector
+    S_crest = np.zeros(nblocks) # spectral centroid
+    for i in range(nblocks):
+        X_fft[i,:] = (np.abs(np.fft.fft(xb[i,:]))[0:flen])/(0.5*b_size)
+        # window fft
+        #X_fft[i,:] = hw*X_fft[i,:]
+        S_crest[i] = (np.max(X_fft[i,:]))/np.sum(X_fft[i,:]) # spectral crest
+    return S_crest
+                                                  ## Plotting and Testing
+
+# create a sample signal
+t = np.arange(0,1,1/fs) # time vector
+x = np.sin(2*np.pi*440*t) #+ np.sin(2*np.pi*880*t) + np.sin(2*np.pi*1320*t)  # signal vector
+
+
+# First block an input signal
+xb, timeInSec = block_audio(x,block_size, hop_size, fs)
+NumOfBlocks = len(timeInSec)
+
+# calculate spectral centroid
+S_cent = extract_spectral_centroid(xb, fs)
+
+# calculate rms_dB
+rms_dB = extract_rms(xb)
 
 
 
@@ -75,7 +109,15 @@ def extract_rms(xb):
 # plot spectral centroid vector
 plt.figure()
 plt.plot(timeInSec, S_cent)
-plt.xlabel('Frequency (Hz)')
+plt.xlabel('Time (sec)')
 plt.ylabel('Magnitude')
 plt.title('Spectral Centroid')
+plt.show(block=False)
+
+# plot RMS Energy
+plt.figure()
+plt.plot(timeInSec, rms_dB)
+plt.xlabel('Time (sec)')
+plt.ylabel('Magnitude')
+plt.title('RMS [dB]')
 plt.show()
